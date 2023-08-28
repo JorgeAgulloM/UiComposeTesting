0ecc21dca1462e7e9729a109e1929edd2b02b2bf
Jorge Agulló <agullojorge@gmail.com>
2023-08-28 19:47:40 +0200

    finders init

--- app/src/androidTest/java/com/softyorch/composetestingui/components/JorgeComponentKtTest.kt
+++ app/src/androidTest/java/com/softyorch/composetestingui/components/JorgeComponentKtTest.kt
@@ -1,18 +1,34 @@
 package com.softyorch.composetestingui.components
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithContentDescription
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
 import org.junit.Test
 
 class JorgeComponentKtTest {
 
     @get:Rule
-    val composableTestRule = createComposeRule()
+    val composeTestRule = createComposeRule()
 
     @Test
     fun myFirstTest() {
-        composableTestRule.setContent {
+        composeTestRule.setContent {
             JorgeComponent()
         }
+
+        //FINDER
+        composeTestRule.onNodeWithText("jorge", ignoreCase = true)
+        composeTestRule.onNodeWithTag("component1")
+        composeTestRule.onNodeWithContentDescription("imageDecorator")
+
+        composeTestRule.onAllNodesWithText("g")
+        composeTestRule.onAllNodesWithTag("component1")
+        composeTestRule.onAllNodesWithContentDescription("imageDecorator")
+
     }
 }
--- app/src/main/java/com/softyorch/composetestingui/components/JorgeComponent.kt
+++ app/src/main/java/com/softyorch/composetestingui/components/JorgeComponent.kt
@@ -1,8 +1,21 @@
 package com.softyorch.composetestingui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 
 @Composable
 fun JorgeComponent() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Jorge", Modifier.testTag("component1"))
+        Text(text = "Agulló", Modifier.testTag("component2"))
 
+        Image(imageVector = Icons.Default.Add, contentDescription = "imageDecorator")
+    }
 }
